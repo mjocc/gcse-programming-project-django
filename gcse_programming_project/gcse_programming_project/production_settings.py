b5e252872c7495f4e044d240f5f2ed060637815d
mjocc <61629073+mjocc@users.noreply.github.com>
2021-08-01 22:21:23 +0100

    Edit production settings

--- gcse_programming_project/gcse_programming_project/production_settings.py
+++ gcse_programming_project/gcse_programming_project/production_settings.py
@@ -1,4 +1,8 @@
 from .settings import *
 
 DEBUG = False
-ALLOWED_HOSTS = ["gcse-programming-project-django.17moccleshaw.repl.co", "c77ee4ac-fda8-4a46-becc-33f285600d8a.id.repl.co"]
+ALLOWED_HOSTS = [
+    ".localhost",
+    "gcse-programming-project-django.17moccleshaw.repl.co",
+    "c77ee4ac-fda8-4a46-becc-33f285600d8a.id.repl.co",
+]
